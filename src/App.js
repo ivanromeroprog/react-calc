2faef4ab9207c387da1e18938b374f51df8db066
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2023-01-02 01:39:44 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import logoApp from './img/Calculator.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor">
+        <h1><img src={logoApp} className="logo" alt='Logo' /> React Calc</h1>
+      </div>
+      <div className="calculadora">
+        
+      </div>
     </div>
   );
 }
